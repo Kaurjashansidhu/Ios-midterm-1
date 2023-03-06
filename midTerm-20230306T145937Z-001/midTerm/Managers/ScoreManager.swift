f6e0d9f0b2e0fc5b043f5f957dd492ef898db185
Kaurjashansidhu <123885037+Kaurjashansidhu@users.noreply.github.com>
2023-03-06 10:47:48 -0500

    Initial commit

---
+++ midTerm-20230306T145937Z-001/midTerm/Managers/ScoreManager.swift
@@ -0,0 +1,16 @@
+//
+//  ScoreManager.swift
+//  ICE_3
+//
+//  Created by Jashandeep Kaur Sidhu-301293237.
+//
+
+import Foundation
+import SpriteKit
+import UIKit
+
+class ScoreManager
+{
+    public static var Score: Int = 0
+    public static var Lives: Int = 5
+}
